7ef3b118b0e52250865d117f100d8389ba2901bf
Derek Anderson <derek@armyofevilrobots.com>
2012-03-25 08:27:53 -0700

    Commit my re-org into libraries.

--- setup.py
+++ setup.py
@@ -3,26 +3,26 @@ import sys, os
 
 version = '0.0'
 
-setup(name='platemaker',
-      version=version,
-      description="",
-      long_description="""\
+setup(name = 'platemaker',
+      version = version,
+      description = "",
+      long_description = """\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='Michael Kessler',
-      author_email='',
-      url='',
-      license='',
-      packages=find_packages('src', exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+      classifiers = [], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords = '',
+      author = 'Michael Kessler',
+      author_email = '',
+      url = '',
+      license = '',
+      packages = find_packages(exclude = ['ez_setup', 'examples', 'tests']),
+      include_package_data = True,
+      zip_safe = False,
+      install_requires = [
           # -*- Extra requirements: -*-
           # I would like to require EITHER pyside or pyqt, but
           # Not sure what everybody prefers.
       ],
-      entry_points="""
+      entry_points = """
       # -*- Entry points: -*-
       """,
       scripts = ['scripts/platemaker',],
